a7ca9bc4bc0b03b77af1e1daa0206223ca605efb
Owen Peterson <owenpeterson27@gmail.com>
2022-02-07 23:34:52 -0500

    Three Tries Version

--- Guess.py
+++ Guess.py
@@ -1,8 +1,21 @@
 secret_word = "giraffe"
 guess = ""
+tries = 0
+tries_limit = 3
+out_of_tries = False
 
-while guess != secret_word:
-    guess = input("Guess the secret word: ")
+while guess != secret_word and not (out_of_tries):
+    if tries < tries_limit:
+        guess = input("Guess the secret word: ")
+        tries += 1
+    else:
+        out_of_tries = True
 
-print("You win!")
+if out_of_tries:
+    print("You're out of tries bucko.")
+else:
+    print("YOU WIN BUCKO!")
+       
+   
+    
 
